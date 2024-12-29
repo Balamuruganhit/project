b786c093a614b2c25d4fb42de52d398ab0e0bf5a
Balamuruganhit <balamuruganb011@gmail.com>
2024-12-29 07:15:02 +0530

    change of the validation correct the misstake and add js

--- ofbiz-framework-release22.01/plugins/auditreport/webapp/AuditReport/static/auditreport.js
+++ ofbiz-framework-release22.01/plugins/auditreport/webapp/AuditReport/static/auditreport.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let formDataArray = [];
   reportField.addEventListener('input', function () { 
     const currentLengthId = reportField.value.length;
-    reportCount.textContent = `${currentLengthId}/250`;
+    reportCount.textContent = `${currentLengthId}/25`;
 
     // Apply red border if limit exceeded, otherwise remove it
     if (currentLengthId > 20) {
